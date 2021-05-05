74a37bcd2571fdbe91755a911bb00ee426066e7e
anandhi85 <anandhi250585@gmail.com>
2021-05-04 21:54:56 -0700

    operators/array

---
+++ array.php
@@ -0,0 +1,30 @@
+<?php
+/* Index Array
+$Dress = array("Saree", "Jeans", "Shirt", "Chudi");
+echo "I Like " .  $Dress [0];*/
+/*
+$Dress = array (
+               0 =>"saree", 
+               1 =>"shirt", 
+               5 =>"Chudi"
+               );
+               print_r($Dress);*/
+               /*associate array
+               $age = array ("anu"=>"30", "ram"=>"25", "sathya"=>"29", "sentha"=>"31");
+               echo "anu age is $age[anu]";*/
+              /* $age = array ("anu"=>"30", "ram"=>"25", "sathya"=>"29", "sentha"=>"31");
+               print_r ($age);*/
+               /*Multidimentional Array */
+               $fruits = array (
+                       "fruits 1" => "apple",
+                       "fruits 2" => "orange", 
+                       "fruits 3" => "grapes", 
+                       "fruits 4" => array(
+                           "mango" => "Alponsa",
+                           "mango1" => "banganapalli",
+                           "Mango" =>"Thai mango"
+                       ),
+                    );
+                /*print_r($fruits);*/
+               
+  ?>
---
+++ operators.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+/*addition
+$x =15;
+$y=5;
+echo $x+$y;*/
+/*Subtraction
+$x=25;
+$y=10;
+echo $x-$y;*/
+/*multiplication
+$x=10;
+$y=58;
+echo $x*$y;*/
+/*Divition
+$x=100;
+$y=50;
+echo $x/$y;*/
+/* modulus
+$x=10;
+$y=3;
+echo $x % $y; */
+/*$x=10;
+$y=5;
+echo $x ** $y;*/
+/*Assignment Operators 
+$x=50;
+echo $x;
+*/
+/*addition
+$x =5;
+$x +=10;
+echo $x;*/
+/*Subtraction
+$x =100;
+$x -=25;
+echo $x;*/
+/* Multiplication 
+$x = 5;
+$x *=5;
+echo $x;
+*/
+/*	Division
+$x =100;
+$x /=5;
+echo $x;*/
+/*Modulus
+$x = 100;
+$x %= 30;
+echo $x;*/
+/*increment/decrement 
+$x =50;
+echo ++$x;*/
+/*$X = 51;
+echo --$X;*/
+/*post
+$x=100;
+echo $x++;*/
+/*Logical Operators
+$x= 20;
+$y= 25;
+ if($x ==20 and $y ==25)
+{
+    echo "x and y is true";
+} */
+/*$x =200;
+$y =250;
+if ($x ==250 or $y=200)
+{
+    echo "x or y is true";
+}
+*/
+/* concatenation
+$x="merge";
+$y = " text";
+echo $x.$y;
+*/
+/*Concatenation assignment Doubt*/
+$x = "Merge";
+$y = " Text";
+$x .= $y
+ echo $x;
+
+?>
+</body>
+</html>
