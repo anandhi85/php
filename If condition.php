7e973c5aafce8dd6db5cfbbcb9de0c8ab5a9f371
anandhi85 <anandhi250585@gmail.com>
2021-05-05 21:38:23 -0700

    elseifelse

---
+++ If condition.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+<?php
+  $x = 10;
+  $b = 10;
+  if($a=$b);
+  echo "condition is tru";
+
+?>
+</body>
+</html>
---
+++ elseifelse.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+<?php
+  $x = 10;
+  $b = 10;
+  if($a!=$b);
+  echo "This condition is false";
+elseif ($a!=b);
+echo "this condition is also false";
+else ($=B);
+echo "This condition is true";
+?>
+</body>
+</html>
+
